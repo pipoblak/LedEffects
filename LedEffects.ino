f5f9fe9fd34799eebf9814c95abe316120a2c6c0
Felipe Ramos <pipoblakgamer@gmail.com>
2016-08-10 11:15:45 -0300

    Working Everything with Program

--- LedEffects.ino
+++ LedEffects.ino
@@ -14,20 +14,20 @@ Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(NUM_LEDS2, PIN2, NEO_GRB + NEO_KHZ8
 boolean ida1=true,ida2=true;
 int i=1;
 int ID=0;
-int EventID1=0,EventID2=0,DELAY1=0,DELAY2=0;
+int EventID1=0,EventID2=0,DELAY1=5,DELAY2=5;
 ThreadController controll = ThreadController();
 Thread threadRead = Thread();
 Thread threadLight = Thread();
 ThreadController groupOfThreads = ThreadController();
 uint16_t contJ1 = 0, contJ2 = 0;
-String Red="0",Green="0",Blue="0",Speed="10";
+String Red1="0",Green1="0",Blue1="0",Red2="0",Green2="0",Blue2="0",Speed="10";
 int contTempo1=0,contTempo2=0;
 boolean staticColor=false,staticColor2=false;
 
 void readCallback(){
 
 if (Serial.available() > 0) {
-  unsigned long currentMillis = millis();
+
   String recivedDataStr="";
   char recivedChar;
   while(Serial.available() > 0){
@@ -49,22 +49,77 @@ if (Serial.available() > 0) {
      int indexR = recivedDataStr.indexOf("R");
      int indexG = recivedDataStr.indexOf("G");
      int indexB = recivedDataStr.indexOf("B");
+     int indexS = recivedDataStr.indexOf("S");
      int indexSize = recivedDataStr.length();
      int str_len = recivedDataStr.length() + 1; 
      char char_array[str_len];
      recivedDataStr.toCharArray(char_array, str_len);
-     Red="";
-     Green="";
-     Blue="";
-     for(int cont=indexR+1;cont<indexG;cont++){
-       Red.concat(char_array[cont]);
+     String idStrip="";
+     for(int cont=indexS+1;cont<indexSize+1;cont++){
+       idStrip.concat(char_array[cont]);
+     }
+     int idStri = idStrip.toInt();
+
+     if(idStri == 0 ) {
+      Red1="";
+      Green1="";
+      Blue1="";
+      for(int cont=indexR+1;cont<indexG;cont++){
+       Red1.concat(char_array[cont]);
+       }
+      for(int cont=indexG+1;cont<indexB;cont++){
+       Green1.concat(char_array[cont]);
+       }
+      for(int cont=indexB+1;cont<indexS;cont++){
+       Blue1.concat(char_array[cont]);
+       }
+      Red2="";
+      Green2="";
+      Blue2="";
+      for(int cont=indexR+1;cont<indexG;cont++){
+       Red2.concat(char_array[cont]);
        }
      for(int cont=indexG+1;cont<indexB;cont++){
-       Green.concat(char_array[cont]);
+       Green2.concat(char_array[cont]);
        }
-     for(int cont=indexB+1;cont<indexSize+1;cont++){
-       Blue.concat(char_array[cont]);
+     for(int cont=indexB+1;cont<indexS;cont++){
+       Blue2.concat(char_array[cont]);
        }
+      }
+     
+     if (idStri == 1){
+      Red1="";
+      Green1="";
+      Blue1="";
+      for(int cont=indexR+1;cont<indexG;cont++){
+       Red1.concat(char_array[cont]);
+       }
+     for(int cont=indexG+1;cont<indexB;cont++){
+       Green1.concat(char_array[cont]);
+       }
+     for(int cont=indexB+1;cont<indexS;cont++){
+       Blue1.concat(char_array[cont]);
+       }
+     }
+     else if (idStri == 2){
+      Red2="";
+      Green2="";
+      Blue2="";
+      
+      for(int cont=indexR+1;cont<indexG;cont++){
+       Red2.concat(char_array[cont]);
+       }
+     for(int cont=indexG+1;cont<indexB;cont++){
+       Green2.concat(char_array[cont]);
+       }
+     for(int cont=indexB+1;cont<indexS;cont++){
+       Blue2.concat(char_array[cont]);
+       }
+
+     }
+     
+     
+  
     }
 
     //LEITURA DE EVENTO
@@ -100,10 +155,12 @@ if (Serial.available() > 0) {
      
      if (idStri == 1){
       EventID1=ID;
+      contTempo1=0;
       DELAY1=Speed.toInt();
      }
      else if (idStri == 2){
       EventID2=ID;
+      contTempo2=0;
       DELAY2=Speed.toInt();
      }
      else if (idStri == 0){
@@ -114,7 +171,7 @@ if (Serial.available() > 0) {
       DELAY1=Speed.toInt();
       DELAY2=Speed.toInt();
       }
-
+    
    // Serial.print("\nEvento "); Serial.print(ID);     Serial.print(" Speed ");     Serial.print(DELAY);       Serial.print(" Strip ");  Serial.print(idStri);     Serial.print(" EventID1 ");     Serial.print(EventID1);     Serial.print(" EventID2 ");     Serial.print(EventID2);
      }
 
@@ -133,7 +190,7 @@ void lightCallback(){
   }
   else if(EventID1==1){
     setStatic(1);
-    staticColorEffect(Red.toInt(),Green.toInt(),Blue.toInt(),1);
+    staticColorEffect(Red1.toInt(),Green1.toInt(),Blue1.toInt(),1);
   }
   
 //FITA 2
@@ -141,9 +198,9 @@ void lightCallback(){
     disableStatic(2);
     rainbowCycleS(DELAY2,2);
   }
-  else if(EventID1==1){
+  else if(EventID2==1){
     setStatic(2);
-    staticColorEffect(Red.toInt(),Green.toInt(),Blue.toInt(),2);
+    staticColorEffect(Red2.toInt(),Green2.toInt(),Blue2.toInt(),2);
   }
 
 
@@ -203,7 +260,7 @@ void setup() {
   threadRead.setInterval(1);
 
   threadLight.onRun(lightCallback);
-  threadLight.setInterval(1);
+  threadLight.setInterval(5);
   
   controll.add(&threadRead);
   
@@ -259,8 +316,15 @@ void setPixel(int Pixel, byte red, byte green, byte blue,int stripID) {
 }
 
 void setAll(byte red, byte green, byte blue,int stripID) {
-  
-  for(int i = 0; i < NUM_LEDS; i++ ) {
+  int numberLeds;
+   
+  if(stripID==1){
+    numberLeds=NUM_LEDS;
+   }
+  if(stripID==2){
+    numberLeds=NUM_LEDS2;
+    }
+  for(int i = 0; i < numberLeds; i++ ) {
     setPixel(i, red, green, blue,stripID); 
   }
   showStrip(stripID);
